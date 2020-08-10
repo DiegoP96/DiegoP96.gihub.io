df02a909d7852f564537bcc5a777ce32ad2fb7cc
MatiasVDB <matiasvdb@hotmail.com>
2020-08-09 23:00:40 -0300

    Boton agregar/ocultar menu

---
+++ js/codigo.js
@@ -0,0 +1,96 @@
+// $(document).ready(function(e) {
+//     $("#listaNachosZapatistas").toggle(
+
+//         function() {
+//             $("#listaNachosZapatistas").hide(e);
+//             $("#texto").hide(e);
+
+//         },
+
+//         $("#nachos").click(function(e) {
+
+
+//             $("#listaNachosZapatistas").show(e);
+//             $("#nachisimos").show(e);
+
+//         }),
+
+//         $("#nachos").click(function(e) {
+
+
+//             $("#listaNachosZapatistas").hide(e);
+//             $("#nachisimos").hide(e);
+
+//         })
+
+
+//     );
+// });
+
+
+
+$(document).ready(function(e) {
+
+    $("#listaNachosZapatistas").toggle(
+
+    );
+    $("#nachisimos").toggle(
+
+    );
+
+});
+
+var contador = 0;
+
+// while (contador != 2) {
+$(document).ready(function(e) {
+
+    $("#nachos").click(function(e) {
+
+        contador++;
+
+        $("#listaNachosZapatistas").show(e);
+        $("#nachisimos").show(e);
+
+        if (contador == 2) {
+
+            $("#listaNachosZapatistas").hide(e);
+            $("#nachisimos").hide(e);
+
+            contador = 0;
+
+        }
+
+    });
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+// }
+
+// $(document).ready(function(e) {
+
+//     if (contador > 2) {
+
+//         $("#nachos").click(function(e) {
+
+
+//             $("#listaNachosZapatistas").hide(e);
+//             $("#nachisimos").hide(e);
+
+//         });
+
+//     }
+
+// });

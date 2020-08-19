77b31258468c4f8b30e2c67c54c7c0db46a9a528
DiegoP96 <diegopaulp@gmail.com>
2020-08-18 23:38:19 -0300

    Update pedido_carrito.js

--- js/pedido_carrito.js
+++ js/pedido_carrito.js
@@ -105,15 +105,6 @@ function quantityChanged(event) {
     updateCartTotal()
 }
 
-function addToCartClicked(event) {
-    var button = event.target
-    var shopItem = button.parentElement.parentElement
-    var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
-    var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
-    var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
-    addItemToCart(title, price, imageSrc)
-    updateCartTotal()
-}
 
 function addItemToCart(title, price) {
 

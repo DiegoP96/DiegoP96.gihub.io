9e1dd7960cd419ad96cf0ae6580295de98d14dad
DiegoP96 <diegopaulp@gmail.com>
2020-10-13 00:08:52 -0300

    a

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-new mysqli('localhost', 'joaking', '#Jj%Fzd#g1tb', 'restaurant2020');
-if (mysqli_connect_error()) {
-    die('Connect Error(' . mysqli_connect_errno() . ')' . mysqli_connect_error());
-}else{
-    echo"hello";
-}
